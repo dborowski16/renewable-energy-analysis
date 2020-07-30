9e3dc6ac600d1d2d24e26c74d5acaf7ceabd8da9
Curtis Caile <Curtis.Caile@du.edu>
2020-07-30 11:09:19 -0600

    updated APIstarter with solar pull

---
+++ curtis_api_keys.py
@@ -0,0 +1,9 @@
+# OpenWeatherMap API Key
+weather_api_key = "e69960cc7ba763e55a6b7fb73788f120"
+
+# Google API Key
+g_key = "AIzaSyB5QyBfI88F5I0RK48jbGuTSvvm2BdgwhE"
+
+
+#NREL API KEY
+nrel_key = "nobOz33O5L39fdZC4qCJOw3Ql3ThzCQ6b4A1Yx42"
