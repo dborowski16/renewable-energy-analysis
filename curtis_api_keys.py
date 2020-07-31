07fc7c6ce34f6571b15a8430a1763e4190d59cd2
Curtis Caile <Curtis.Caile@du.edu>
2020-07-31 08:52:01 -0600

    Secondary data table csvs being pushed

--- curtis_api_keys.py
+++ curtis_api_keys.py
@@ -4,6 +4,9 @@ weather_api_key = "e69960cc7ba763e55a6b7fb73788f120"
 # Google API Key
 g_key = "AIzaSyB5QyBfI88F5I0RK48jbGuTSvvm2BdgwhE"
 
+#NREL API KEY Dave
+nrel_key = "9bH6FAyLRnkdgVWfFiNbIhFhULWAJOc6n7cOe3pE"
+
 
 # #NREL API KEY Curtis
 # nrel_key = "nobOz33O5L39fdZC4qCJOw3Ql3ThzCQ6b4A1Yx42"
@@ -13,5 +16,6 @@ g_key = "AIzaSyB5QyBfI88F5I0RK48jbGuTSvvm2BdgwhE"
 # nrel_key = "qBvAmHMh0HYOU2goxPsWWFYxc5adueX99kVIFbbn"
 
 
-#NREL API KEY Brooke
-nrel_key = "5BaZKgdCC826scuoyvQ2WCl1yckzWzEndkiNId7m"
+# #NREL API KEY Brooke
+# nrel_key = "5BaZKgdCC826scuoyvQ2WCl1yckzWzEndkiNId7m"
+
